32f7b7609071a0b941161a4f70474f7ef02470f6
prabithasujith <51877066+prabithasujith@users.noreply.github.com>
2020-09-08 07:49:06 +0100

    Initial commit

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            slider.Value = 0.5;
+
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            valueOfSlider.Text = String.Format("Value is {0:F2}", e.NewValue);
+        }
+    }
+}
